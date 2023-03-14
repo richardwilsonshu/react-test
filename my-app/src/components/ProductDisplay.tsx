0ad2b8508c06f17b929b2d485d30f42ab7b50686
Richard Wilson <richard.wilson@and.digital>
2023-03-14 12:08:43 +0000

    Add hovering conditional rendering

--- my-app/src/components/ProductDisplay.tsx
+++ my-app/src/components/ProductDisplay.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Card, Col } from "react-bootstrap";
 import { IProduct } from "../class/IProduct";
 
@@ -7,17 +8,22 @@ export type ProductDisplayProps = {
 };
 
 export const ProductDisplay = ({product, onRemove}: ProductDisplayProps) => {
+
+    const [ hover, setHover ] = useState(false);
+
     return (
         <Col sm={3}>
-            <Card>
+            <Card onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                 <Card.Body>
                     <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>{product.description}</Card.Text>
-                    <Card.Text>£{product.price}</Card.Text>
-                    <p className="m-0 p-0 small text-muted">Category: {product.category}</p>
-                    <p className="m-0 p-0 small text-muted">Keywords: {product.keywords}</p>
-                    <Button className="mt-3" variant="danger" onClick={onRemove}>Remove</Button>
+                    <div style={{"display": (hover ? "block" : "none")}}>
+                        <Card.Text>{product.description}</Card.Text>
+                        <Card.Text>£{product.price}</Card.Text>
+                        <p className="m-0 p-0 small text-muted">Category: {product.category}</p>
+                        <p className="m-0 p-0 small text-muted">Keywords: {product.keywords}</p>
+                        <Button className="mt-3" variant="danger" onClick={onRemove}>Remove</Button>
+                    </div>
                 </Card.Body>
             </Card>
         </Col>
